710b0cde0e478718de3b8eecc7d771b4b6a110b5
Naresh <nareshkumarthodupunoori@gmail.com>
2024-07-03 10:57:22 +0530

    Added get, push, put and delete

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+app.use(express.json());
 
 app.get('/', (req, res) => {
     res.send('Hello World');
@@ -15,6 +16,53 @@ app.get('/courses',(req,res) => {
     res.json(courses);
 });
 
-app.listen(3001, () => {
-    console.log('Listening on port 3000');
+app.post('/courses',(req,res) => {
+    // console.log(req.body);
+    const course = {
+        id: courses.length + 1,
+        name: req.body.name
+    };
+    courses.push(course);
+    res.send(course);
+});
+
+app.put('/courses/:id',(req,res) =>{
+    try{
+        let singlecourse = courses.find((course) => {
+            return course.id === +req.params.id;
+        });
+
+        if(!singlecourse){
+            res.status(404).send('course does not exist');
+        }
+
+        singlecourse.name = req.body.name;
+        res.send(courses);
+    }
+    catch(err){
+        res.status(500).send(err);
+    }
+});
+
+app.delete('/courses/:id',(req,res) => {
+    try{
+        let singlecourse = courses.find((course) => {
+            return course.id === +req.params.id;
+        });
+
+        if(!singlecourse){
+            res.status(404).send('course does not exist');
+        }
+
+        const index = courses.indexOf(singlecourse);
+        courses.splice(index,1);
+        res.send(courses);
+    }
+    catch(err){
+        res.status(500).send(err);
+    }
+})
+
+app.listen(3000, () => {
+    console.log('Server started');
 });
